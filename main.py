8a1405adff48ac297bbb7745288d31cd1b806e98
Makoto Shirose <makoto.shirose@gmail.com>
2021-05-29 13:08:24 +0900

    scrape added

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from bs4 import BeautifulSoup
+import requests
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -19,6 +22,44 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+url = 'https://news.yahoo.co.jp/articles/d56ceef87e48131e6c409aee300b942fd943860a'
+res = requests.get(url)
+
+if res.status_code==200:
+
+  soup = BeautifulSoup(res.text, 'html.parser')
+  title_text = soup.find('h1', class_='sc-exkUMo').get_text()
+  time_text = soup.find('time').get_text()
+  body_text = soup.find('div', class_='article_body')
+  body_text = str(body_text)
+  body_text = body_text.replace("\"","\\\"")
+  body_text = body_text.replace("\n", "<br>")
+  body_text = body_text.replace("<br><br>", "<br>")
+
+  content =  '{'
+  content += ' \"status\": \"OK\", '
+  content += ' \"totalResults\": \"1\", '
+  content += ' \"articles\": [ '
+  content += ' { '
+  content += ' \"source\": { '
+  content += ' \"id\": null, '
+  content += ' \"name\": "Yahoo.co.jp" '
+  content += ' }, '
+  content += ' \"author\": null,'
+  content += ' \"title\": \"' + title_text + '\", '
+  content += ' \"description\": \"' + body_text + '\",'
+  content += ' \"url\": \"' + url + '\",'
+  content += ' \"urlToImage\": null,'
+  content += ' \"publishedAt\": null,'
+  content += ' \"content\": null}]'
+  content += '}'
+
+  
+  
+elif res.status_code==404:
+  print('NOT OK:' + url)
+
 
 @app.route("/")
 def hello_world():
@@ -46,10 +87,14 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=title_text))
 
 
 if __name__ == "__main__":
 #    app.run()
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+
+
+
